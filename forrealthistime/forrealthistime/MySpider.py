5cad103e3f8ae5f7d5dc38219d046a023ba8899b
Patrick Wamsley <pwamsley2015@francisparker.org>
2020-12-23 14:54:17 -0800

    working hello world spider

--- forrealthistime/forrealthistime/MySpider.py
+++ forrealthistime/forrealthistime/MySpider.py
@@ -1,13 +1,18 @@
 
-import Scrapy
+import scrapy
+import string
 
-class MySpider(scrapy.spider):
+class MySpider(scrapy.Spider):
     name = 'spood'
-    start_urls = ['https://www.kbb.com/cars-for-sale/cars/used-cars/?p=1&color=']
+    start_urls = ['http://quotes.toscrape.com/tag/humor/']
 
     def parse(self, response):
-        print(response)
-        next_page = response.css('li.next a::attr("href")').get()
+        for quote in response.css('div.quote'):
+            yield {
+                'author': quote.xpath('span/small/text()').get(),
+                'text': quote.css('span.text::text').get(),
+            }
 
+        next_page = response.css('li.next a::attr("href")').get()
         if next_page is not None:
             yield response.follow(next_page, self.parse)
